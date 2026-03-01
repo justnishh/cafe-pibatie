c9e72e1d101bf554fa42972e17b330648cce0a78
justnishh <nishantnakum44@gmail.com>
2026-03-01 13:50:14 +0530

    Clean up code and remove loading animation

--- test-traffic.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Sending 200 requests to test Google Analytics...
-for /L %%i in (1,1,200) do (
-    curl -s -o nul "https://cafe-pibatie.onrender.com"
-    if %%i %% 20 == 0 echo %%i requests sent...
-)
-echo Done! 200 requests sent.
-pause
